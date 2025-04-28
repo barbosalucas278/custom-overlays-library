f0a97aff920d7d21c598a0b87ffdc9e0ab34efc3
Lucas Barbosa <barbosalucasroberto@gmail.com>
2025-04-27 23:57:10 -0300

    fix: update Babel configuration and Rollup setup for better compatibility

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,24 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import terser from '@rollup/plugin-terser';
 import babel from '@rollup/plugin-babel';
+import terser from 'rollup-plugin-terser';
 
 export default {
-    input: 'src/index.js',
-    output: [
-        {
-            file: 'dist/index.js',
-            format: 'cjs',
-            sourcemap: true,
-        },
-        {
-            file: 'dist/index.esm.js',
-            format: 'esm',
-            sourcemap: true,
-        },
-    ],
-    plugins: [
-        peerDepsExternal(),
-        resolve({
-            extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        }),
-        babel({
-            babelHelpers: 'bundled',
-            presets: ['@babel/preset-react'], // Transforma JSX
-            extensions: ['.js', '.jsx', '.ts', '.tsx'], // Procesa estos archivos
-        }),
-        commonjs(),
-        terser()],
+  input: 'src/index.js',
+  output: [
+    { file: 'dist/index.js', format: 'cjs', sourcemap: true },
+    { file: 'dist/index.esm.js', format: 'esm', sourcemap: true },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+    terser(),
+  ],
 };
